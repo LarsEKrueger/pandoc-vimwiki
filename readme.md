a744218d7e442f3f8d932a175cb7410574f14dca
Lars Krueger <lars_e_krueger@gmx.de>
2017-05-21 19:08:19 +0200

    functional: convert checkboxes to unicode

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ This modules extends pandoc to support certain elements from vimwiki's markup.
 
 # Requirements
 
-* [ ] Convert ticked (U+2611) and unticked (U+2610) boxes to unicode characters
-* [ ] Handle transclusions using double curly parentheses
-* [ ] Emojis
+* [X] Convert ticked (U+2611) and unticked (U+2610) boxes to unicode characters
+* [ ] Handle transclusions using double curly brackets
+* [ ] Handle transclusions using double square brackets
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,42 @@
-module Main where
+module Main
+where
+
+import Text.Pandoc.JSON as J
+import Control.Monad
+import Data.Char
+import System.IO
+import qualified Data.ByteString.Lazy as BL
+import Data.List
+
+prefixAndReplacement :: [([Inline],String)]
+prefixAndReplacement =
+  [ ( [Str "[", J.Space, Str "]"] , "☐")
+  , ( [Str "[.]"]                 , "▄⃞▄")
+  , ( [Str "[o]"]                 , "▆⃞▆")
+  , ( [Str "[O]"]                 , "▇⃞▇")
+  , ( [Str "[X]"]                 , "☑")
+  ]
+
+-- Replace the first matching prefix
+replacePrefixes :: [Inline] -> [([Inline],String)] -> [Inline]
+replacePrefixes s [] = s
+replacePrefixes s ((p,r):pars) =
+  if p `isPrefixOf` s
+     then Str r : drop (length p) s
+     else replacePrefixes s pars
+
+inlineItems :: Inline -> Inline
+inlineItems (Emph s)    = Emph $ replacePrefixes s prefixAndReplacement
+inlineItems (Strong s)  = Strong $ replacePrefixes s prefixAndReplacement
+inlineItems (Strikeout s)  = Strikeout $ replacePrefixes s prefixAndReplacement
+inlineItems (Superscript s)  = Superscript $ replacePrefixes s prefixAndReplacement
+inlineItems (Subscript s)  = Subscript $ replacePrefixes s prefixAndReplacement
+inlineItems (SmallCaps s)  = SmallCaps $ replacePrefixes s prefixAndReplacement
+inlineItems x = x
+
+inlineBlocks :: Block -> Block
+inlineBlocks (Plain s) = Plain $ replacePrefixes (map inlineItems s) prefixAndReplacement
+inlineBlocks x = x
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = toJSONFilter inlineBlocks
