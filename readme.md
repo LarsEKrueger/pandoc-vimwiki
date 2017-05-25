085ec1bf86f479f6c5754a1dcd8dd02ccfc4ccdc
Lars Krueger <lars_e_krueger@gmx.de>
2017-05-25 17:57:51 +0200

    functional: add transclusion
    Handles transclusion at top-level only as everything else needs to be done
    inside the markdown parser.

--- readme.md
+++ readme.md
@@ -5,6 +5,6 @@ This modules extends pandoc to support certain elements from vimwiki's markup.
 # Requirements
 
 * [X] Convert ticked (U+2611) and unticked (U+2610) boxes to unicode characters
-* [ ] Handle transclusions using double curly brackets
+* [X] Handle transclusions using double curly brackets
 * [ ] Handle transclusions using double square brackets
 
--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module Main
 where
 
@@ -6,10 +7,12 @@ import Text.Pandoc.Walk
 import Control.Monad
 import Data.Char
 import System.IO
+import System.IO.Error
+import Control.Exception.Base
 import qualified Data.ByteString.Lazy as BL
 import Data.List
 
-
+import Text.Pandoc
 import Text.Pandoc.Definition
 import Text.Pandoc.Generic
 import Data.Aeson
@@ -31,22 +34,82 @@ replacePrefixes s ((p,r):pars) =
      then Str r : drop (length p) s
      else replacePrefixes s pars
 
-inlineItems :: Inline -> Inline
-inlineItems (Emph s)    = Emph $ replacePrefixes s prefixAndReplacement
-inlineItems (Strong s)  = Strong $ replacePrefixes s prefixAndReplacement
-inlineItems (Strikeout s)  = Strikeout $ replacePrefixes s prefixAndReplacement
-inlineItems (Superscript s)  = Superscript $ replacePrefixes s prefixAndReplacement
-inlineItems (Subscript s)  = Subscript $ replacePrefixes s prefixAndReplacement
-inlineItems (SmallCaps s)  = SmallCaps $ replacePrefixes s prefixAndReplacement
-inlineItems x = x
+inlineItems :: Inline -> IO Inline
+inlineItems (Emph s)    = return $ Emph $ replacePrefixes s prefixAndReplacement
+inlineItems (Strong s)  = return $ Strong $ replacePrefixes s prefixAndReplacement
+inlineItems (Strikeout s)  = return $ Strikeout $ replacePrefixes s prefixAndReplacement
+inlineItems (Superscript s)  = return $ Superscript $ replacePrefixes s prefixAndReplacement
+inlineItems (Subscript s)  = return $ Subscript $ replacePrefixes s prefixAndReplacement
+inlineItems (SmallCaps s)  = return $ SmallCaps $ replacePrefixes s prefixAndReplacement
+inlineItems x = return x
 
 inlineBlocks :: Block -> IO Block
-inlineBlocks (Plain s) = return $ Plain $ replacePrefixes (map inlineItems s) prefixAndReplacement
+inlineBlocks (Plain s) = do
+  ili <- mapM inlineItems s
+  return $ Plain $ replacePrefixes ili prefixAndReplacement
 inlineBlocks x = return x
 
+isTransclusion :: String -> Bool
+isTransclusion s = isPrefixOf "{{" s && isSuffixOf "}}" s
+
+extractLink :: String -> String
+extractLink s = drop 2 $ take (length s - 2) s
+
+extensions :: [String]
+extensions =
+  [ ".md"
+  , ".markdown"
+  , ".wiki"
+  , ".mdwiki"
+  ]
+
+
+tryFirst :: [a] -> (a -> IO (Maybe b)) -> IO (Maybe b)
+tryFirst [] _ = return Nothing
+tryFirst (a:as) f = do
+  mbOk <- f a
+  case mbOk of
+       Just _ -> return mbOk
+       Nothing -> tryFirst as f
+
+justReadFile :: String -> IO (Maybe [Block])
+justReadFile fn = bracket (openFile fn ReadMode) hClose $ \handle -> do
+  hSetEncoding handle utf8
+  !cont <- hGetContents handle
+  case readMarkdown def cont of
+      Left _ -> return Nothing
+      Right (Pandoc _ blocks) -> return $ Just blocks
+
+handleTransclusion :: String -> IO [Block]
+handleTransclusion s = do
+  let filename = extractLink s
+  mbBlocks <- tryFirst extensions $ \ext -> do
+    hPutStrLn stderr $ "Trying to load file «" ++ filename ++ ext ++ "»"
+    catchIOError (justReadFile $ filename ++ ext) (\_ -> return Nothing)
+  case mbBlocks of
+       Nothing -> return [Para [Str $ "Error: Could not transclude «" ++ filename ++ "»"]]
+       Just blocks -> return blocks
+
+processLinks :: Block -> IO [Block]
+processLinks (Para [Str s]) =
+  if isTransclusion s
+     then handleTransclusion s
+     else return [Para [Str s]]
+processLinks x = return [x]
+
+concatMapM :: Monad m => (a -> m [b]) -> [a] -> m [b]
+concatMapM f as = do
+  bbs <- mapM f as
+  return $ concat bbs
+
+transclude :: Pandoc -> IO Pandoc
+transclude (Pandoc m bs) = do
+  walked <- concatMapM processLinks bs
+  return $ Pandoc m walked
+
 main :: IO ()
-main = do
-  input <- BL.getContents
-  filtered <- (walkM inlineBlocks :: Pandoc -> IO Pandoc) $ either error id $ eitherDecode' input
-  BL.putStr $ encode filtered
+main = BL.getContents >>=
+  (walkM inlineBlocks :: Pandoc -> IO Pandoc) . either error id . eitherDecode' >>=
+  transclude >>=
+  BL.putStr . encode
 
